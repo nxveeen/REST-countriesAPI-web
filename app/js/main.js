a492479abc5ca86f83073efa7ce40dfa4eb71540
nxveeen <nilotpalgoyal9393@gmail.com>
2021-08-05 20:48:47 +0530

    dropdown for filter by region implemented

--- app/js/main.js
+++ app/js/main.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log("hello");
+// console.log("hello");
+
+const btnDropdown = document.querySelector("#dropdownBtn");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+const chevronIcon = document.querySelector(".bi.bi-chevron-down");
+
+btnDropdown.addEventListener("click", function (e) {
+  dropdownMenu.classList.toggle("hidden");
+  chevronIcon.classList.toggle("rotate");
+});
+
+document.addEventListener("keydown", function (e) {
+  if (e.key === "Escape") {
+    chevronIcon.classList.remove("rotate");
+    dropdownMenu.classList.add("hidden");
+  }
+});
