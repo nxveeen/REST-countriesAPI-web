64ab5b5aaaff03131642601a42e5e7e30cc82a5d
nxveeen <nilotpalgoyal9393@gmail.com>
2021-08-06 14:37:45 +0530

    theme switch funtionality added

--- app/js/main.js
+++ app/js/main.js
@@ -4,7 +4,18 @@
 const btnDropdown = document.querySelector("#dropdownBtn");
 const dropdownMenu = document.querySelector(".dropdown-menu");
 const chevronIcon = document.querySelector(".bi.bi-chevron-down");
+const themeSwitchBtn = document.querySelector("#theme__switch");
 
+themeSwitchBtn.addEventListener("click", function (e) {
+  console.log(e.target.closest(".theme__switch"));
+  //  { okay, okay2 } = e.target.closest(".theme__switch").children;
+
+  // console.log(okay, okay2);
+  // console.log(e.target.firstElementChild);
+  document.body.classList.toggle("dark-theme");
+});
+
+//dropdown mechanism
 btnDropdown.addEventListener("click", function (e) {
   dropdownMenu.classList.toggle("hidden");
   chevronIcon.classList.toggle("rotate");
